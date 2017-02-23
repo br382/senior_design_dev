554f7adda10af48b4278e3ae54f1eeb47582f099
br382 <br382@drexel.edu>
2017-02-23 13:15:44 -0500

    Missing setuptools installation command.

--- code/tools/APK Tools/install_buildozer.sh
+++ code/tools/APK Tools/install_buildozer.sh
@@ -1,16 +1,9 @@
-# Install Buildozer enviroment on Vanilla Mint 17.2 x64
-sudo apt-get install git -y
+# Install Buildozer enviroment on Vanilla Mint 18.1 x64
 sudo apt-get install python-pip -y
+sudo pip install --upgrade pip
+sudo pip install setuptools
+sudo pip install --upgrade setuptools
 sudo pip install buildozer
-#my_dir="$(pwd)"
-#cd $HOME
-#git clone https://github.com/kivy/buildozer.git
-#cd buildozer
-#sudo python setup.py install
-#cd ..
-#sudo rm -rf ./buildozer/
-#cd $my_dir
-#cd ..
 # Required for Buildozer's compiling/packaging sequence:
 sudo apt-get install python-pip python-dev libxml2-dev libxslt1-dev zlib1g-dev -y
 #sudo pip uninstall Cython -y
